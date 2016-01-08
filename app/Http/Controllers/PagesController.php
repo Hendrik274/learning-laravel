387d2c2e182527e0b3c54dba365206ea43b665c7
Hendrik274 <oberholzer274@gmail.com>
2016-01-07 18:34:20 -0800

    Finished videos 1-5

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    public function about() {
+
+        $people = ['Taylor Otwell', 'Dayl Rees'];
+
+        return view('/about', compact('people'));
+    }
+
+
+    public function contact() {
+        return view('/contact');
+    }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    @if (Session::has('flash_message'))
+        <div class ="alert alert-success"></div>
+    @endif
+    @yield('content')
+</div>
+
+@yield('footer')
+
+</body>
+
+</html>
